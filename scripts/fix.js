ccd9aaf257b2ba2ba756132cf3640ea30dcad6b7
Diego M. Rodrigues <diegomr86@gmail.com>
2022-03-08 17:45:45 -0300

    Adiciona script de migração do conteúdo das páginas

--- scripts/fix.js
+++ scripts/fix.js
@@ -1,7 +1,44 @@
 require('../api/database')
+const mongoose = require('mongoose')
+const Page = mongoose.model('Page')
 
-const fix = () => {
-  // await fixAttachments()
+const fixPageContents = async () => {
+  const pages = await Page.find()
+  for (const page of pages) {
+    // eslint-disable-next-line no-console
+    console.log(page.content && (!page.sections || page.sections.length === 0))
+    if (page.content && (!page.sections || page.sections.length === 0)) {
+      page.sections.push({
+        background: null,
+        attrs: {
+          fluid: false
+        },
+        columns: [
+          {
+            background: null,
+            attrs: {
+              lg: '12'
+            },
+            components: [
+              {
+                type: 'p',
+                attrs: {
+                  class: ''
+                },
+                content: page.content,
+                category: 'paragraph'
+              }
+            ]
+          }
+        ]
+      })
+      await page.save()
+    }
+  }
+}
+
+const fix = async () => {
+  await fixPageContents()
   process.exit()
 }
 
