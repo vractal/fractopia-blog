c21ee050207e7e9dd27e877a6eebdb4312ed5513
Diego M. Rodrigues <diegomr86@gmail.com>
2022-04-03 20:32:39 -0300

    fix: medias list image alignment

--- components/Medias.vue
+++ components/Medias.vue
@@ -2,10 +2,10 @@
   <div class="medias-component">
     <div>
       <ul class="list-unstyled">
-        <b-media v-for="media in medias" :key="media._id" tag="li" class="border-top py-3">
-          <template #aside>
-            <b-img v-if="media.image || media.oembed_thumb" :src="media.image ? media.image.thumb : media.oembed_thumb" :alt="media.title" />
-          </template>
+        <div v-for="media in medias" :key="media._id" tag="li" class="border-top py-3">
+          <div v-if="media.image || media.oembed_thumb" class="mb-3">
+            <b-img :src="media.image ? media.image.thumb : media.oembed_thumb" :alt="media.title" />
+          </div>
           <b-badge v-if="media.type" class="mb-2">{{ media.type }}</b-badge>
           <p class="mb-0">
             <n-link :to="'/biblioteca/' + media._id"><strong>{{ media.title }}</strong></n-link>
@@ -13,7 +13,7 @@
           <p class="mb-0">
             <small>{{ media.description | truncate(200) }}</small>
           </p>
-        </b-media>
+        </div>
       </ul>
     </div>
   </div>
