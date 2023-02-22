8cdd0b7d57752bac2dc81fcca725f790dddab97b
vractal <victor@jigvis.com.br>
2023-02-22 18:41:54 -0300

    Show tags in blog page

--- components/Tags.vue
+++ components/Tags.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <b-badge v-for="tag in tags" :key="tag" pill variant="primary" class="pointer mr-1" @click="$emit('click', tag)">{{ tag }}</b-badge>
+    <b-badge pill :variant="selected === '' ? 'success' : 'primary'" class="pointer mr-1" @click="$emit('click', '')">Todas</b-badge>
+    <b-badge v-for="tag in tags" :key="tag" pill :variant="selected !== tag ? 'primary' : 'success'" class="pointer mr-1" @click="$emit('click', tag)">{{ tag }}</b-badge>
   </div>
 </template>
 
@@ -10,7 +11,16 @@ export default {
     tags: {
       type: Array,
       default: () => []
+    },
+    selected: {
+      type: String,
+      default: ''
     }
   }
 }
 </script>
+<style>
+.active-tag {
+  background-color: #6acf26 !important;
+}
+</style>
--- pages/noticias/index.vue
+++ pages/noticias/index.vue
@@ -6,10 +6,10 @@
     <section class="content pb-5">
       <Page slug="noticias" :breadcrumb="false" />
       <b-container>
-        <tags :to="$route.path" />
-        <div v-if="posts">
-          <Posts :posts="posts" />
-          <div v-if="posts.length === 0" class="alert text-center">Nenhuma notícia encontrada</div>
+        <Tags :selected="selectedTag" :tags="availableTags" @click="handleTagSelection" />
+        <div v-if="filteredPosts">
+          <Posts :posts="filteredPosts" />
+          <div v-if="filteredPosts.length === 0" class="alert text-center">Nenhuma notícia encontrada</div>
           <!-- <share /> -->
         </div>
       </b-container>
@@ -34,10 +34,62 @@ export default {
         return this.settings.features.posts.title
       }
       return features.posts.title
+    },
+    availableTags() {
+      // TODO: This is still not getting from the API all available tags, not sure how to handle this.
+      if (!this.posts) {
+        return []
+      }
+      const tags = new Set()
+      this.posts.forEach(post => {
+        post.tags.forEach(tag => tags.add(tag))
+      })
+      return Array.from(tags).sort()
+    },
+    filteredPosts() {
+      if (!this.posts) {
+        return []
+      }
+      if (!this.selectedTag) {
+        return this.posts
+      } else {
+        return this.posts.filter(post => {
+          return post.tags.includes(this.selectedTag)
+        })
+      }
+    },
+    selectedTag() {
+      return this.$route.query.tag ? this.$route.query.tag
+        : ''
+    }
+  },
+  watch: {
+    '$route.params.tag'(newValue, oldValue) {
+      // Recalculate filteredPosts when the tag parameter changes
+      if (newValue !== oldValue) {
+        this.$nextTick(() => {
+          this.selectedTag()
+        })
+      }
     }
   },
   async created() {
     this.posts = await this.$axios.$get('/api/posts', { params: this.$route.query })
+  },
+  methods: {
+    handleTagSelection(tag) {
+      // Get the current route query parameters
+      const query = { ...this.$route.query }
+
+      if (tag !== '') {
+        query.tag = tag
+      } else {
+        delete query.tag
+      }
+
+      // Change the route to include the new query parameters
+      this.$router.push({ path: this.$route.path, query })
+    }
   }
 }
 </script>
